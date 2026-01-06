b24077fc69ad471ec02267b0fc14d3657b1e8447
Sriharshareddy6464 <adapalasriharshareddy@gmail.com>
2026-01-06 21:07:53 +0530

    test: verify CI/CD auto deploy

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"status": "ProdDeploy API is running"}
+    return {"status": "ProdDeploy API is LIVE from CI/CD"}
 
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
